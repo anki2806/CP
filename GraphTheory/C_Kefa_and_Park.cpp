e8b9e31bfd71a8d1d0b4798ed6ff278082c34865
Ankit <ankit80519@gmail.com>
2026-02-13 16:26:13 +0530

    Redid the file

--- GraphTheory/C_Kefa_and_Park.cpp
+++ GraphTheory/C_Kefa_and_Park.cpp
@@ -1,50 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int ans = 0, m;
-vector<int> edges[10000005];
-int arr[10000005];
-void dfs(int ind, int currCat, int parent) {
 
-    if(arr[ind] == 1) {
-        currCat++;
-    }
-    else {
-        currCat = 0;
-    }
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define ANKIT ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-    if(currCat > m) {
-        return;
-    }
-
-    bool isLeaf = true;
-    for(int i = 0; i < edges[ind].size(); i++) {
-        if(edges[ind][i] != parent) {
-            isLeaf = false;
-            dfs(edges[ind][i], currCat, ind);
+int dfs(vector<int> &v, vector<vector<int>> &arr, int ind, int sum, int m, int parent) {
+    if(arr[ind].size() == 1 && arr[ind][0] == parent) {
+        // cout<<ind<<" vind "<<v[ind]<<" ";
+        if(m < sum + v[ind]) {
+            return 0;
         }
+        return 1;
+    }
+    if(v[ind] == 0) {
+        sum = 0;
+    }
+    sum += v[ind];
+    if(m < sum) {
+        return 0;
+    }
+    
+    int val = 0;
+    for(int i = 0; i < arr[ind].size(); i++) {
+        if(arr[ind][i] != parent) {
+            val += dfs(v, arr, arr[ind][i], sum, m, ind);
+        }
+        
+    }
+    // cout<<"VAL = "<<val<<" ";
+    return val;
+}
+void solve() {
+    int n, m;
+    cin>>n>>m;
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin>>v[i];
     }
 
-    if(isLeaf) {
-        ans++;
+    vector<vector<int>> arr(n, vector<int>());
+
+    for(int i = 0; i < n - 1; i++) {
+        int a, b;
+        cin>>a>>b;
+
+        arr[a - 1].push_back(b - 1);
+        arr[b - 1].push_back(a - 1);
     }
+    
+    int ans = dfs(v, arr, 0, 0, m, -1);
+    cout<<ans<<"\n";
 }
 
 int main() {
-    int n, b;
-    cin>>n>>b;
+    ANKIT
 
-    for(int i = 0; i < n; i++) {
-        cin>>arr[i];
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
     }
-    for(int i = 0; i < n - 1; i++) {
-        int u, v;
-        cin>>u>>v;
-        edges[u - 1].push_back(v - 1);
-        edges[v - 1].push_back(u - 1);
-    }
-
-    m = b;
-    dfs(0, 0, -1);
-    cout<<ans<<"\n"; 
-
+    return 0;
 }
